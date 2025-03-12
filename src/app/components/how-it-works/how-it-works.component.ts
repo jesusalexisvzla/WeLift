dfbd81790477f75d1dc2701f14428b5da606f2e6
Alexis Valenzuela <jesusalexisvzla@gmail.com>
2025-03-12 02:02:20 -0700

    added page two and three to hiw

--- src/app/components/how-it-works/how-it-works.component.ts
+++ src/app/components/how-it-works/how-it-works.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HowItWorksComponent implements OnInit {
 
+  statusOne = false;
+  statusTwo = false;
+  statusThree = false;
+  statusFour = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  open(variableName) {
+    this[variableName] = !this[variableName]
+    console.log(this.statusOne)
   }
 
 }
