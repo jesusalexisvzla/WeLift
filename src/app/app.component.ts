ab18162f470a4993381519bced042c3197db3b82
Alexis Valenzuela <jesusalexisvzla@gmail.com>
2025-04-14 16:12:18 -0700

    styles for design not being affected by the virtual keyboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'plantillaFrontend';
+
+  ngOnInit(): void {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
 }
